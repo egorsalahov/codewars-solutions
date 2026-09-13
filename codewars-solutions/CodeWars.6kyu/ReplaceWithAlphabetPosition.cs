12353bea57ddbcbac2c5ccd368de375a96e410b2
EgorSalahov <egorsalahov2@gmail.com>
2026-09-13 16:28:15 +0300

    new solution

---
+++ codewars-solutions/CodeWars.6kyu/ReplaceWithAlphabetPosition.cs
@@ -0,0 +1,38 @@
+﻿//https://www.codewars.com/kata/546f922b54af40e1e90001da
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codewars_solutions.CodeWars._6kyu
+{
+    public class ReplaceWithAlphabetPosition
+    {
+        public static string Solution(string text)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            text = text.ToLower();
+
+            foreach (char c in text)
+            {
+                int code = (int)c;
+
+                if (code < 97 || code > 122) continue;
+                else
+                {
+                    int forResult = c - 'a' + 1;
+
+                    sb.Append($"{forResult.ToString()} ");
+                }
+            }
+
+            if (sb.Length > 1)
+            {
+                sb.Remove(sb.Length - 1, 1);
+            }
+
+            return sb.ToString();
+        }
+    }
+}

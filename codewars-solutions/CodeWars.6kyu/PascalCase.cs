2830237f83f4dab8741d8ac64979de484fac59c2
EgorSalahov <egorsalahov2@gmail.com>
2026-09-13 14:29:03 +0300

    new solution

---
+++ codewars-solutions/CodeWars.6kyu/PascalCase.cs
@@ -0,0 +1,30 @@
+﻿//https://www.codewars.com/kata/587731fda577b3d1b0001196
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codewars_solutions.CodeWars._6kyu
+{
+    public class PascalCase
+    {
+        public static string Solution(string str)
+        {
+            string[] words = str.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            StringBuilder resultBuilder = new StringBuilder();
+
+            foreach (var word in words)
+            {
+                StringBuilder sb = new StringBuilder(word);
+
+                char firstLetter = sb[0];
+                sb.Remove(0, 1);
+                sb.Insert(0, char.ToUpper(firstLetter));
+
+                resultBuilder.Append(sb.ToString());
+            }
+
+            return resultBuilder.ToString();
+        }
+    }
+}
